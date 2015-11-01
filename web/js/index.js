a45585ff6a39309ea505888ac180293e96a34753
knowthis <admin@zhouxianbao.cn>
2015-11-01 17:35:26 +0800

    finish index reactjs

--- web/js/index.js
+++ web/js/index.js
@@ -46,27 +46,7 @@ var LeftShare = React.createClass({
         )
     }
 });
-var Data ={
-    personInfo:{
-        personPic:'img/index.png',
-        personName:'笨笨时光机'
-    },
-    list:[
-        {id:1,link:'#',text:'首页'},
-        {id:2,link:'#',text:'最新内容'},
-        {id:3,link:'#',text:'项目列表'},
-        {id:4,link:'#',text:'个人简历'}
-    ],
-    copyright: {
-        copyInfo: '笨笨时光',
-        copyNum: '鄂ICP备14002646-1',
-        share: [
-            {id:1,link: '#', text: '&#xe601;'},
-            {id:2,link: '#', text: '&#xe600;'},
-            {id:3,link: '#', text: '&#xe602;'}
-        ]
-    }
-};
+
 
 var LeftCopyright = React.createClass({
     render: function () {
@@ -97,11 +77,6 @@ var LeftNavMain = React.createClass({
 });
 
 //右部
-var rightDate = {
-    news:[
-        {id:1,newsPic:'img/index.png',newsTitle:'我会想你的',newsSee:'000',newsCreateTime:'2015-12-23',newsAuthor:'admin',newsContent:'在计算机程序或文本编辑中，硬编码是指将可变变量用一个固定值来代替的方法。用这种方法编译后，如果以后需要更改此变量就非常困难了。大部分程序语言里，可以将一个固定数值定义为一个标记，然用这个特殊标记来取代变量名称。当标记名称改变时，变量名不变，这样，当重新编译整个程序时，变量都不再是固定值，这样就更容易的实现了改变变量的目的。'}
-    ]
-};
 var RightItem = React.createClass({
     render: function () {
         return (
@@ -126,9 +101,7 @@ var RightMain = React.createClass({
     render: function () {
         var itemNode = this.props.data.news.map(function (value) {
             return (
-
                 <RightItem data={value}  key={value.id}/>
-
             )
         });
         return (
@@ -142,12 +115,88 @@ var RightMain = React.createClass({
         )
     }
 });
-var Main = React.createClass({
+
+//博客内容组件
+var blog = React.createClass({
     render: function () {
         return (
+            <div class="blog_main">
+                <div class="blog_header">
+                    <h1>{this.props.data.newsTitle}</h1>
+                    <div class="blog_info">
+                        浏览数：<span>{this.props.data.newsTitle}</span>
+                        作者：<span>{this.props.data.newsAdmin}</span>
+                        评论数：<span>{this.props.data.newsCommentNum}</span>
+                        创建时间：<span>{this.props.data.newsCreateTime}</span>
+                        修改时间：<span>{this.props.data.newsChangeTime}</span></div>
+                </div>
+                <div class="blog_body">
+                    {this.props.data.newsContent}
+                </div>
+            </div>
+        )
+    }
+});
+var Main = React.createClass({
+    getInitialState: function() {
+       // console.log(this.props.url);
+        return {
+            data: {
+                Data: {
+                    personInfo: {
+                        personPic: 'img/index.png',
+                        personName: '笨笨时光机'
+                    },
+                    list: [
+                        {id: 1, link: '#', text: '首页'}
+                    ],
+                    copyright: {
+                        copyInfo: '笨笨时光',
+                        copyNum: '鄂ICP备14002646-1',
+                        share: [
+                            {id: 1, link: '#', text: '&#xe601;'}
+                        ]
+                    }
+                },
+                rightDate: {
+                    news: [
+                        {
+                            id: 1,
+                            newsPic: 'img/index.png',
+                            newsTitle: '我会想你的',
+                            newsSee: '000',
+                            newsCreateTime: '2015-12-23',
+                            newsAuthor: 'admin',
+                            newsContent: '在计算机程序或文本编辑中，硬编码是指将可变变量用一个固定值来代替的方法。用这种方法编译后，如果以后需要更改此变量就非常困难了。大部分程序语言里，可以将一个固定数值定义为一个标记，然用这个特殊标记来取代变量名称。当标记名称改变时，变量名不变，这样，当重新编译整个程序时，变量都不再是固定值，这样就更容易的实现了改变变量的目的。'
+                        }
+                    ]
+                }
+            }
+        };
+    },
+    componentDidMount: function() {
+      //  console.log(this.props.url);
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            type:'get',
+            cache: false,
+            success: function(value) {
+                console.log(value[0]);
+                this.setState({data:{Data: value[0].Data,rightDate:value[0].rightDate}});
+
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    render: function () {
+       // console.log(this.state.data.Data);
+        return (
             <div className="container">
-                <LeftNavMain data={this.props.data.Data} />
-                <RightMain data={this.props.data.rightDate} />
+                <LeftNavMain data={this.state.data.Data} />
+                <RightMain data={this.state.data.rightDate} />
                 <div className="index_bg"></div>
             </div>
         )
@@ -156,39 +205,39 @@ var Main = React.createClass({
 
 
 var data={
-    Data:{
-        personInfo:{
-            personPic:'img/index.png',
-            personName:'笨笨时光机'
-        },
-        list:[
-            {id:1,link:'#',text:'首页'},
-            {id:2,link:'#',text:'最新内容'},
-            {id:3,link:'#',text:'项目列表'},
-            {id:4,link:'#',text:'个人简历'}
-        ],
-        copyright: {
-            copyInfo: '笨笨时光',
-            copyNum: '鄂ICP备14002646-1',
-            share: [
-                {id:1,link: '#', text: '&#xe601;'},
-                {id:2,link: '#', text: '&#xe600;'},
-                {id:3,link: '#', text: '&#xe602;'}
-            ]
-        }
-    },
-    rightDate:{
-        news:[
-            {id:1,newsPic:'img/index.png',newsTitle:'我会想你的',newsSee:'000',newsCreateTime:'2015-12-23',newsAuthor:'admin',newsContent:'在计算机程序或文本编辑中，硬编码是指将可变变量用一个固定值来代替的方法。用这种方法编译后，如果以后需要更改此变量就非常困难了。大部分程序语言里，可以将一个固定数值定义为一个标记，然用这个特殊标记来取代变量名称。当标记名称改变时，变量名不变，这样，当重新编译整个程序时，变量都不再是固定值，这样就更容易的实现了改变变量的目的。'},
-            {id:2,newsPic:'img/index.png',newsTitle:'我会想你的',newsSee:'000',newsCreateTime:'2015-12-23',newsAuthor:'admin',newsContent:'在计算机程序或文本编辑中，硬编码是指将可变变量用一个固定值来代替的方法。用这种方法编译后，如果以后需要更改此变量就非常困难了。大部分程序语言里，可以将一个固定数值定义为一个标记，然用这个特殊标记来取代变量名称。当标记名称改变时，变量名不变，这样，当重新编译整个程序时，变量都不再是固定值，这样就更容易的实现了改变变量的目的。'},
-            {id:3,newsPic:'img/index.png',newsTitle:'我会想你的',newsSee:'000',newsCreateTime:'2015-12-23',newsAuthor:'admin',newsContent:'在计算机程序或文本编辑中，硬编码是指将可变变量用一个固定值来代替的方法。用这种方法编译后，如果以后需要更改此变量就非常困难了。大部分程序语言里，可以将一个固定数值定义为一个标记，然用这个特殊标记来取代变量名称。当标记名称改变时，变量名不变，这样，当重新编译整个程序时，变量都不再是固定值，这样就更容易的实现了改变变量的目的。'},
-            {id:4,newsPic:'img/index.png',newsTitle:'我会想你的',newsSee:'000',newsCreateTime:'2015-12-23',newsAuthor:'admin',newsContent:'在计算机程序或文本编辑中，硬编码是指将可变变量用一个固定值来代替的方法。用这种方法编译后，如果以后需要更改此变量就非常困难了。大部分程序语言里，可以将一个固定数值定义为一个标记，然用这个特殊标记来取代变量名称。当标记名称改变时，变量名不变，这样，当重新编译整个程序时，变量都不再是固定值，这样就更容易的实现了改变变量的目的。'},
-            {id:5,newsPic:'img/index.png',newsTitle:'我会想你的',newsSee:'000',newsCreateTime:'2015-12-23',newsAuthor:'admin',newsContent:'在计算机程序或文本编辑中，硬编码是指将可变变量用一个固定值来代替的方法。用这种方法编译后，如果以后需要更改此变量就非常困难了。大部分程序语言里，可以将一个固定数值定义为一个标记，然用这个特殊标记来取代变量名称。当标记名称改变时，变量名不变，这样，当重新编译整个程序时，变量都不再是固定值，这样就更容易的实现了改变变量的目的。'},
-            {id:6,newsPic:'img/index.png',newsTitle:'我会想你的',newsSee:'000',newsCreateTime:'2015-12-23',newsAuthor:'admin',newsContent:'在计算机程序或文本编辑中，硬编码是指将可变变量用一个固定值来代替的方法。用这种方法编译后，如果以后需要更改此变量就非常困难了。大部分程序语言里，可以将一个固定数值定义为一个标记，然用这个特殊标记来取代变量名称。当标记名称改变时，变量名不变，这样，当重新编译整个程序时，变量都不再是固定值，这样就更容易的实现了改变变量的目的。'}
-        ]
-    }
+   Data:{
+       personInfo:{
+           personPic:'img/index.png',
+           personName:'笨笨时光机'
+       },
+       list:[
+           {id:1,link:'#',text:'首页'},
+           {id:2,link:'#',text:'最新内容'},
+           {id:3,link:'#',text:'项目列表'},
+           {id:4,link:'#',text:'个人简历'}
+       ],
+       copyright: {
+           copyInfo: '笨笨时光',
+           copyNum: '鄂ICP备14002646-1',
+           share: [
+               {id:1,link: '#', text: '&#xe601;'},
+               {id:2,link: '#', text: '&#xe600;'},
+               {id:3,link: '#', text: '&#xe602;'}
+           ]
+       }
+   },
+   rightDate:{
+       news:[
+           {id:1,newsPic:'img/index.png',newsTitle:'我会想你的',newsSee:'000',newsCreateTime:'2015-12-23',newsAuthor:'admin',newsContent:'在计算机程序或文本编辑中，硬编码是指将可变变量用一个固定值来代替的方法。用这种方法编译后，如果以后需要更改此变量就非常困难了。大部分程序语言里，可以将一个固定数值定义为一个标记，然用这个特殊标记来取代变量名称。当标记名称改变时，变量名不变，这样，当重新编译整个程序时，变量都不再是固定值，这样就更容易的实现了改变变量的目的。'},
+           {id:2,newsPic:'img/index.png',newsTitle:'我会想你的',newsSee:'000',newsCreateTime:'2015-12-23',newsAuthor:'admin',newsContent:'在计算机程序或文本编辑中，硬编码是指将可变变量用一个固定值来代替的方法。用这种方法编译后，如果以后需要更改此变量就非常困难了。大部分程序语言里，可以将一个固定数值定义为一个标记，然用这个特殊标记来取代变量名称。当标记名称改变时，变量名不变，这样，当重新编译整个程序时，变量都不再是固定值，这样就更容易的实现了改变变量的目的。'},
+           {id:3,newsPic:'img/index.png',newsTitle:'我会想你的',newsSee:'000',newsCreateTime:'2015-12-23',newsAuthor:'admin',newsContent:'在计算机程序或文本编辑中，硬编码是指将可变变量用一个固定值来代替的方法。用这种方法编译后，如果以后需要更改此变量就非常困难了。大部分程序语言里，可以将一个固定数值定义为一个标记，然用这个特殊标记来取代变量名称。当标记名称改变时，变量名不变，这样，当重新编译整个程序时，变量都不再是固定值，这样就更容易的实现了改变变量的目的。'},
+           {id:4,newsPic:'img/index.png',newsTitle:'我会想你的',newsSee:'000',newsCreateTime:'2015-12-23',newsAuthor:'admin',newsContent:'在计算机程序或文本编辑中，硬编码是指将可变变量用一个固定值来代替的方法。用这种方法编译后，如果以后需要更改此变量就非常困难了。大部分程序语言里，可以将一个固定数值定义为一个标记，然用这个特殊标记来取代变量名称。当标记名称改变时，变量名不变，这样，当重新编译整个程序时，变量都不再是固定值，这样就更容易的实现了改变变量的目的。'},
+           {id:5,newsPic:'img/index.png',newsTitle:'我会想你的',newsSee:'000',newsCreateTime:'2015-12-23',newsAuthor:'admin',newsContent:'在计算机程序或文本编辑中，硬编码是指将可变变量用一个固定值来代替的方法。用这种方法编译后，如果以后需要更改此变量就非常困难了。大部分程序语言里，可以将一个固定数值定义为一个标记，然用这个特殊标记来取代变量名称。当标记名称改变时，变量名不变，这样，当重新编译整个程序时，变量都不再是固定值，这样就更容易的实现了改变变量的目的。'},
+           {id:6,newsPic:'img/index.png',newsTitle:'我会想你的',newsSee:'000',newsCreateTime:'2015-12-23',newsAuthor:'admin',newsContent:'在计算机程序或文本编辑中，硬编码是指将可变变量用一个固定值来代替的方法。用这种方法编译后，如果以后需要更改此变量就非常困难了。大部分程序语言里，可以将一个固定数值定义为一个标记，然用这个特殊标记来取代变量名称。当标记名称改变时，变量名不变，这样，当重新编译整个程序时，变量都不再是固定值，这样就更容易的实现了改变变量的目的。'}
+       ]
+   }
 };
 ReactDOM.render(
-    <Main data={data} />,
+    <Main url="server/hello.json"  />,
     document.getElementById("contain")
 );
